fb07648a4e00d6a6c11806e47f92e5bd79731b15
yixuannliu <yixuann.liu.utoronto@gmail.com>
2020-08-02 16:22:31 -0400

    Initial Commit

---
+++ SetGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame
+//
+//  Created by Cosmos Liu on 2020-08-02.
+//  Copyright © 2020 Cosmos Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
